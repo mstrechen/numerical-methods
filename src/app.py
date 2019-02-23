e36bfa57bf019616c27e0cde2e1d206f61cdc9fd
Matvii Strechen <mvstrechen@gmail.com>
2019-02-23 21:34:47 +0200

    Fix debug flag

--- src/app.py
+++ src/app.py
@@ -6,10 +6,5 @@ from config import configure_app
 
 app = configure_app()
 
-def get_bool_flag(key):
-    val = os.environ.get(key, False)
-    return val in ['1', 'true', 'True', 'TRUE']
-
 if __name__ == '__main__':
-    debug = get_bool_flag('DEBUG')
-    app.run(debug=debug, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 from api import api
@@ -5,10 +7,16 @@ from pages import pages
 
 from werkzeug.contrib.fixers import ProxyFix
 
+def get_bool_flag(key):
+    val = os.environ.get(key, False)
+    return val in ['1', 'true', 'True', 'TRUE']
+
 def configure_app():
     app = Flask(__name__, static_url_path='/static', static_folder='static')
     app.register_blueprint(api.bp)
     app.register_blueprint(pages.bp)
     app.wsgi_app = ProxyFix(app.wsgi_app)
+
+    app.config['DEBUG'] = get_bool_flag('DEBUG')
 
     return app
