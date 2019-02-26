4a188c9e04ed2741fcafccbe77a5627289d6523a
Matvii Strechen <mvstrechen@gmail.com>
2019-02-26 20:21:24 +0200

    Add dichotomy-method page (now is empty, will be developed later)

--- src/pages/pages.py
+++ src/pages/pages.py
@@ -2,11 +2,21 @@ from flask import abort, Blueprint, render_template
 
 bp = Blueprint('pages', __name__, template_folder='templates') 
 
+pages_list = [
+    {
+        "page" : "dichotomy-method",
+        "headline" : "Dichotomy (Bisection method)",
+        "text" : "Dichotomy (or Bisection) method is a root-finding method that applies to any continuous functions for which one knows two values with opposite signs.",
+    },
+]
 
 @bp.route('/', defaults={'page': 'index'})
 @bp.route('/<page>')
 def get_page(page):
     try:
-        return render_template('%s.html' % page, page_title="Main page")
+        if page == 'index':
+            return render_template('index.html', pages=pages_list)
+        else:
+            return render_template('%s.html' % page)
     except:
         abort(404)
